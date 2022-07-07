81a975b3bb14ff9e440e6b08d422f073facf2ea3
Kamil Chlebek <kamil.chlebek94@gmail.com>
2022-07-07 20:03:26 +0200

    Force chunk for standalone component

--- src/app/lazy-route/lazy-standalone/lazy-standalone.component.ts
+++ src/app/lazy-route/lazy-standalone/lazy-standalone.component.ts
@@ -1,9 +1,11 @@
 import { Component, Inject, OnInit } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { LazyService } from '../lazy.service'
+import { FormsModule } from '@angular/forms'
 
 @Component({
-  imports: [CommonModule],
+  // FormsModule just to force another chunk for `app-lazy-standalone` otherwise it would be merged with common.js
+  imports: [CommonModule, FormsModule],
   selector: 'app-lazy-standalone',
   templateUrl: './lazy-standalone.component.html',
   styleUrls: ['./lazy-standalone.component.css'],
